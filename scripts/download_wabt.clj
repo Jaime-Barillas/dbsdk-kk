36284a8aa4ec20eb6f338920e6651d1fa69eb7d7
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2023-07-14 19:28:15 -0400

    Replace nushell scripts with Babashka scripts

---
+++ scripts/download_wabt.clj
@@ -0,0 +1,37 @@
+(ns download-wabt
+  (:require
+    [babashka.fs :as fs]
+    [babashka.http-client :as http]
+    [babashka.process :as proc]))
+
+(defn- get-os []
+  (let [os (System/getProperty "os.name")]
+    (cond
+      (re-find #"Windows" os) "windows"
+      (re-find #"Linux" os) "ubuntu")))
+
+(defn -main [& args]
+  ;; Prep Work
+  (let [version "1.0.33"
+        os (get-os)
+        download-url (str "https://github.com/WebAssembly/wabt/releases/download/" version "/wabt-" version "-" os ".tar.gz")
+        download-dir (-> *file* fs/parent fs/parent (fs/path "lib" "wabt"))
+        archive (fs/path download-dir "wabt.tar.gz")
+        extracted-dir (fs/path download-dir (str "wabt-" version))]
+
+    (when-not os
+      (println "Unsupported OS:" os)
+      (System/exit 1))
+
+    ;; Download and Extract
+    (fs/create-dirs download-dir)
+    (when (or (not (fs/exists? archive)) (some #{"-f" "--force"} args))
+      (println "Downloading WABT...")
+      (->> (http/get download-url {:as :bytes})
+        :body
+        (fs/write-bytes archive)))
+
+    (println "Extracting WABT...")
+    (proc/shell {:dir (str download-dir)} "tar" "-xf" (str archive))
+    (fs/copy-tree (fs/path extracted-dir "bin") (fs/path download-dir "bin") {:replace-existing true})
+    (fs/delete-tree extracted-dir)))
