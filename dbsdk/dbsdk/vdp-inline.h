cbdb9788532cfc6553d48b435aa8213fcd22d857
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2023-08-29 16:24:48 -0400

    [wip]: db_vdp.h part 2

--- dbsdk/dbsdk/vdp-inline.h
+++ dbsdk/dbsdk/vdp-inline.h
@@ -12,6 +12,22 @@ static inline kk_unit_t dbsdk_vdp__vdp_depthFunc(uint32_t comparison) {
   vdp_depthFunc(comparison);
   return kk_Unit;
 }
+static inline kk_unit_t dbsdk_vdp__vdp_blendEquation(uint32_t mode) {
+  vdp_blendEquation(mode);
+  return kk_Unit;
+}
+static inline kk_unit_t dbsdk_vdp__vdp_blendFunc(uint32_t srcFactor, uint32_t dstFactor) {
+  vdp_blendFunc(srcFactor, dstFactor);
+  return kk_Unit;
+}
+static inline kk_unit_t dbsdk_vdp__vdp_setWinding(uint32_t windingOrder) {
+  vdp_setWinding(windingOrder);
+  return kk_Unit;
+}
+static inline kk_unit_t dbsdk_vdp__vdp_setCulling(uint8_t enabled) {
+  vdp_setCulling(enabled);
+  return kk_Unit;
+}
 
 kk_unit_t kk_dbsdk_vdp__vdp_setVsyncHandler(kk_function_t, kk_context_t*);
 
